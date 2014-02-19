4642e731fc6be624fdab0f8358c4d00bdd683888
Stevan <stevan@stevan-Inspiron-1545.(none)>
2014-02-19 20:51:22 +0100

    Contiki-initial commit

---
+++ cpu/cc2538/dev/sht11_sensor.c
@@ -0,0 +1,334 @@
+     /***********************************************************************************
+Project:          SHT1x/7x demo program (V2.4)
+Filename:         SHT1x_sample_code.c    
+
+Prozessor:        80C51 family
+Compiler:         Keil Version 6.23a
+
+Autor:            MST
+Copyrigth:        (c) Sensirion AG      
+***********************************************************************************/
+// Revisions:
+// V2.4	 calc_sht11()       Coefficients for humidity and temperature conversion 
+//                          changed (for V4 sensors)
+//       calc_dewpoint()	New formula for dew point calculation 
+ 
+#include "contiki.h"
+#include "dev/i2c.h"
+#include "sys/energest.h"
+#include "dev/sys-ctrl.h"
+#include "dev/ioc.h"
+#include "dev/gpio.h"
+#include "reg.h"
+#include "sht11_sensor.h"
+#include <math.h>    
+#include <stdio.h> 
+#include <string.h>
+#include <stdint.h>
+
+#if STARTUP_CONF_VERBOSE
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
+void gpio_test(void)
+{
+	GPIO_SET_OUTPUT(GPIO_C_BASE, 0x01);
+	GPIO_CLR_PIN(GPIO_C_BASE, 0x01);
+
+	GPIO_SET_OUTPUT(GPIO_C_BASE, 0x02);
+	GPIO_SET_PIN(GPIO_C_BASE, 0x02);
+
+	GPIO_SET_OUTPUT(GPIO_C_BASE, 0x04);
+	GPIO_SET_PIN(GPIO_C_BASE, 0x04);
+
+	GPIO_SET_OUTPUT(GPIO_C_BASE, 0x08);
+	GPIO_SET_PIN(GPIO_C_BASE, 0x08);
+}
+
+// writes a byte on the Sensibus and checks the acknowledge 
+char s_write_byte(unsigned char value)
+{
+  unsigned char i,error=0;
+  unsigned char val;
+
+  val = value;
+
+  // Set CLK pin as output
+  GPIO_SET_OUTPUT(SCK_BASE, SCK);
+  // Set data pin as output
+  GPIO_SET_OUTPUT(DATA_BASE, DATA);
+    
+  //for (i=0x80; i>0; i/=2)                       //shift bit for masking
+  for (i=0; i<8; i++, val <<= 1)
+  { 
+	  if ((val & 0x80) == 0x80)
+		  GPIO_SET_PIN(GPIO_B_BASE, DATA);
+	  else
+		  GPIO_CLR_PIN(GPIO_B_BASE, DATA);
+    //if (i & value)                            //masking value with i , write to SENSI-BUS
+    //	GPIO_SET_PIN(GPIO_B_BASE, DATA);
+    //else
+    //	GPIO_CLR_PIN(GPIO_B_BASE, DATA);
+                             
+    clock_delay_usec(2);                      //observe setup time
+    GPIO_SET_PIN(GPIO_B_BASE, SCK);         //clk for SENSI-BUS
+    clock_delay_usec(5);                      //pulswith approx. 5 us  	
+    GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+    clock_delay_usec(2);                      //observe hold time
+  }
+  
+  GPIO_SET_PIN(GPIO_B_BASE, DATA);          //release DATA-line
+  clock_delay_usec(2);                        //observe setup time
+  GPIO_SET_PIN(GPIO_B_BASE, SCK);          //clk #9 for ack
+  
+  // Set data pin as input
+  GPIO_SET_INPUT(DATA_BASE, DATA);
+
+  error = GPIO_READ_PIN(GPIO_B_BASE, DATA) >> DATA_PIN;    //check ack (DATA will be pulled down by SHT11)
+
+  PRINTF("Dataaaaaaaaaaaaa pin = 0x%x\n", GPIO_READ_PIN(GPIO_B_BASE, DATA));
+  PRINTF("error = 0x%x\n", error);
+
+  GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+
+  return error;                                               //error=1 in case of no acknowledge
+}
+
+// reads a byte form the Sensibus and gives an acknowledge in case of "ack=1" 
+char s_read_byte(unsigned char ack)
+{ 
+  unsigned char i,val=0;
+
+  // Set CLK pin as output
+  GPIO_SET_OUTPUT(SCK_BASE, SCK);
+  // Set data pin as output
+  GPIO_SET_OUTPUT(DATA_BASE, DATA);
+
+  GPIO_SET_PIN(GPIO_B_BASE, DATA);                          //release DATA-line
+  
+  // Set data pin as input
+  GPIO_SET_INPUT(DATA_BASE, DATA);
+  
+  GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+  clock_delay_usec(2);
+
+  for (i=0x80; i>0; i/=2)                                       //shift bit for masking
+  { 
+	  	GPIO_SET_PIN(GPIO_B_BASE, SCK);                           //clk for SENSI-BUS
+    
+		if (GPIO_READ_PIN(GPIO_B_BASE, DATA) == DATA)            //read bit
+		  val=(val | i);
+
+		GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+		clock_delay_usec(2);
+  }
+
+  // Set data pin as output
+  GPIO_SET_OUTPUT(DATA_BASE, DATA);
+
+  //in case of "ack==1" pull down DATA-Line
+  if (ack == 1)
+	  GPIO_CLR_PIN(GPIO_B_BASE, DATA);
+  else
+	  GPIO_SET_PIN(GPIO_B_BASE, DATA);
+  clock_delay_usec(2);                                          //observe setup time
+
+  GPIO_SET_PIN(GPIO_B_BASE, SCK);                             //clk #9 for ack
+  clock_delay_usec(5);                                          //pulswith approx. 5 us 
+  GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+  clock_delay_usec(2);                                          //observe hold time						    
+  GPIO_SET_PIN(GPIO_B_BASE, DATA);                          //release DATA-line
+  
+  return val;
+}
+
+// generates a transmission start 
+//       _____         ________
+// DATA:      |_______|
+//           ___     ___
+// SCK : ___|   |___|   |______
+void s_transstart(void)
+{
+   // Set CLK pin as output
+   GPIO_SET_OUTPUT(SCK_BASE, SCK);
+   // Set data pin as output
+   GPIO_SET_OUTPUT(DATA_BASE, DATA);
+  
+   GPIO_SET_PIN(GPIO_B_BASE, DATA);        //Initial state
+   GPIO_CLR_PIN(GPIO_B_BASE, SCK);         //Initial state
+   clock_delay_usec(2);
+   
+   GPIO_SET_PIN(GPIO_B_BASE, SCK);
+   clock_delay_usec(2);
+   GPIO_CLR_PIN(GPIO_B_BASE, DATA);
+   clock_delay_usec(2);
+   GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+   clock_delay_usec(5);
+   GPIO_SET_PIN(GPIO_B_BASE, SCK);
+   clock_delay_usec(2);
+   GPIO_SET_PIN(GPIO_B_BASE, DATA);
+   clock_delay_usec(2);
+   GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+}
+
+// communication reset: DATA-line=1 and at least 9 SCK cycles followed by transstart
+//       _____________________________________________________         ________
+// DATA:                                                      |_______|
+//          _    _    _    _    _    _    _    _    _        ___     ___
+// SCK : __| |__| |__| |__| |__| |__| |__| |__| |__| |______|   |___|   |______
+void s_connectionreset(void)
+{  
+  unsigned char i; 
+
+  // Set CLK pin as output
+  GPIO_SET_OUTPUT(SCK_BASE, SCK);
+  // Set data pin as output
+  GPIO_SET_OUTPUT(DATA_BASE, DATA);
+  
+  GPIO_SET_PIN(GPIO_B_BASE, DATA);        //Initial state
+  GPIO_CLR_PIN(GPIO_B_BASE, SCK);         //Initial state
+   
+  for(i=0; i<9; i++)                         //9 SCK cycles
+  { 
+	  GPIO_SET_PIN(GPIO_B_BASE, SCK);
+	  GPIO_CLR_PIN(GPIO_B_BASE, SCK);
+  }
+  
+  s_transstart();                            //transmission start
+}
+
+// resets the sensor by a softreset 
+char s_softreset(void)
+{ 
+  unsigned char error=0; 
+   
+  s_connectionreset();              //reset communication
+  error += s_write_byte(RESET);       //send RESET-command to sensor
+  
+  return error;                     //error=1 in case of no response form the sensor
+}
+
+// reads the status register with checksum (8-bit)
+char s_read_statusreg(unsigned char *p_value, unsigned char *p_checksum)
+{ 
+  unsigned char error=0;
+  
+  s_transstart();                   //transmission start
+  error=s_write_byte(STATUS_REG_R); //send command to sensor
+  *p_value=s_read_byte(ACK);        //read status register (8-bit)
+  *p_checksum=s_read_byte(noACK);   //read checksum (8-bit)  
+  
+  return error;                     //error=1 in case of no response form the sensor
+}
+
+// writes the status register with checksum (8-bit)
+char s_write_statusreg(unsigned char *p_value)
+{ 
+  unsigned char error=0;
+  
+  s_transstart();                   //transmission start
+  error+=s_write_byte(STATUS_REG_W);//send command to sensor
+  error+=s_write_byte(*p_value);    //send value of status register
+  
+  return error;                     //error>=1 in case of no response form the sensor
+}
+ 							   
+// makes a measurement (humidity/temperature) with checksum
+char s_measure(uint16_t *p_value, unsigned char *p_checksum, unsigned char mode)
+{ 
+  unsigned char error=0;
+  unsigned int i;
+  unsigned char data_low, data_high;
+
+  s_transstart();                   //transmission start
+  switch(mode)                      //send command to sensor
+  {                     
+    case TEMP: 
+      error+=s_write_byte(MEASURE_TEMP); 
+    break;
+    
+    case HUMI: 
+      error+=s_write_byte(MEASURE_HUMI); 
+    break;
+    
+    default: 
+    break;	 
+  }
+  
+  for (i=0; i<65535; i++)     //wait until sensor has finished the measurement
+  {
+    if (GPIO_READ_PIN(GPIO_B_BASE, DATA) == 0)
+      break; 
+  }
+  
+  if (GPIO_READ_PIN(GPIO_B_BASE, DATA) == DATA)         // or timeout (~2 sec.) is reached
+    error+=1; 
+                   
+  data_high = s_read_byte(ACK);    //read the first byte (MSB)
+  data_low = s_read_byte(ACK);    //read the second byte (LSB)
+  *p_checksum = s_read_byte(noACK);  //read checksum
+  
+  if (mode == TEMP)
+  {
+	  *p_value = data_high;
+	  *p_value <<= 6;
+	  *p_value |= (data_low & 0x3F);
+  }
+
+  if (mode == HUMI)
+  {
+	  *p_value = data_high;
+	  *p_value <<= 4;
+	  *p_value |= (data_low & 0x0F);
+  }
+
+  return error;
+}
+
+// calculates temperature [\B0C] and humidity [%RH] 
+// input :  humi [Ticks] (12 bit) 
+//          temp [Ticks] (14 bit)
+// output:  humi [%RH]
+//          temp [\B0C]
+void calc_sth11(float *p_humidity ,float *p_temperature)
+{ 
+  const float C1=-2.0468;           // for 12 Bit RH
+  const float C2=+0.0367;           // for 12 Bit RH
+  const float C3=-0.0000015955;     // for 12 Bit RH
+  const float T1=+0.01;             // for 12 Bit RH
+  const float T2=+0.00008;          // for 12 Bit RH	
+
+  float rh=*p_humidity;             // rh:      Humidity [Ticks] 12 Bit 
+  float t=*p_temperature;           // t:       Temperature [Ticks] 14 Bit
+  float rh_lin;                     // rh_lin:  Humidity linear
+  float rh_true;                    // rh_true: Temperature compensated humidity
+  float t_C;                        // t_C   :  Temperature [\B0C]
+
+  t_C = t*0.01 - 40.1;                    //calc. temperature [\B0C] from 14 bit temp. ticks @ 5V
+  rh_lin = C3*rh*rh + C2*rh + C1;         //calc. humidity from ticks to [%RH]
+  rh_true = (t_C-25)*(T1+T2*rh)+rh_lin;   //calc. temperature compensated humidity [%RH]
+  
+  if(rh_true>100)                   //cut if the value is outside of
+    rh_true = 100;
+    
+  if(rh_true<0.1)                   //the physical possible range
+    rh_true = 0.1;
+
+  *p_temperature = t_C;             //return temperature [\B0C]
+  *p_humidity = rh_true;            //return humidity[%RH]
+}
+
+// calculates dew point
+// input:   humidity [%RH], temperature [\B0C]
+// output:  dew point [\B0C]
+float calc_dewpoint(float h,float t)
+{ 
+  float k, dew_point ;
+  
+  k = (log10(h)-2)/0.4343 + (17.62*t)/(243.12+t);
+  dew_point = 243.12*k/(17.62-k);
+  
+  return dew_point;
+}
---
+++ cpu/cc2538/dev/sht11_sensor.h
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2012, Texas Instruments Incorporated - http://www.ti.com/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * \addtogroup cc2538
+ * @{
+ *
+ * \defgroup cc2538-sht11 cc2538 sht11
+ *
+ * Driver for the cc2538 sht11 sensor
+ * @{
+ *
+ * \file
+ * Header file for the cc2538 sht11 driver
+ *
+ * \author
+ *          Stevan Marinkovic <marinkovic.stevan@gmail.com>
+ */
+#ifndef SHT11_H_
+#define SHT11_H_
+
+/*
+typedef union
+{ unsigned int i;
+  float f;
+} value;
+*/
+
+//----------------------------------------------------------------------------------
+// modul-var
+//----------------------------------------------------------------------------------
+#define TEMP	0
+#define HUMI	1
+
+#define	DATA_BASE     GPIO_B_BASE
+#define	SCK_BASE      GPIO_B_BASE
+#define DATA_PIN     3
+#define SCK_PIN      5
+#define	 DATA   	  0x08
+#define	 SCK   	      0x20
+
+#define	 PB_2   	  0x04
+#define PB_4   	      0x10
+
+#define noACK 0
+#define ACK   1
+                            //adr  command  r/w
+#define STATUS_REG_W 0x06   //000   0011    0
+#define STATUS_REG_R 0x07   //000   0011    1
+#define MEASURE_TEMP 0x03   //000   0001    1
+#define MEASURE_HUMI 0x05   //000   0010    1
+#define RESET        0x1e   //000   1111    0
+
+// Function prototypes
+
+char s_write_byte(unsigned char value);
+
+char s_read_byte(unsigned char ack);
+
+void s_transstart(void);
+
+void s_connectionreset(void);
+
+char s_softreset(void);
+
+char s_read_statusreg(unsigned char *p_value, unsigned char *p_checksum);
+
+char s_write_statusreg(unsigned char *p_value);
+
+char s_measure(uint16_t *p_value, unsigned char *p_checksum, unsigned char mode);
+
+void calc_sth11(float *p_humidity ,float *p_temperature);
+
+float calc_dewpoint(float h,float t);
+
+void gpio_test(void);
+
+
+
+#endif /* SHT11_H_ */
+
+
+/**
+ * @}
+ * @}
+ */
